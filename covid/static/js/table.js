f9447a40afc22ed4e56e15aebade7add8f42ed4f
Alex <Smirkyredpanda@gmail.com>
2021-02-10 08:03:50 -0500

    Update documentation

--- covid/static/js/table.js
+++ covid/static/js/table.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         "info":     false,
         "filter":   false,
         ajax: {
-            url: "https://ivizri.com/covid/static/api/daily_death_dates.json",
+            url: "https://ivizri.com/covid/static/api/values/daily_death_dates.json",
             dataSrc: '',
             method: "GET",
             dataType: 'json'
@@ -24,7 +24,7 @@ $(document).ready(function () {
         "info":     false,
         "filter":   false,
         ajax: {
-            url: "https://ivizri.com/covid/static/api/capacity.json",
+            url: "https://ivizri.com/covid/static/api/values/capacity.json",
             dataSrc: '',
             method: "GET",
             dataType: 'json'
@@ -34,24 +34,6 @@ $(document).ready(function () {
             { data: "%_capacity_covid", className: "text-right" }
         ]
     });
-
-    $('#nursingHome').DataTable({
-        "searching": false,
-        "paging": false,
-        "info": false,
-        ajax: {
-            url: "http://127.1.2.3:5000/static/api/nursing_home_table.json",
-            dataSrc: '',
-            method: "GET",
-            dataType: 'json'
-        },
-        columns: [
-            { data: "city_town" },
-            { data: "facility_name" },
-            { data: "cases_avg", className: "text-right" },
-            { data: "fatalities_avg", className: "text-right" }
-        ]
-    });   
     
     $("#schoolsTable").DataTable({
         "searching": false,
