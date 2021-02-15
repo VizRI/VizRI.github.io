a44a3544598deb6f11b9a7cedfcbf9246e523755
Alex <Smirkyredpanda@gmail.com>
2021-02-15 17:14:41 -0500

    Update documentation

--- covid/static/js/table.js
+++ covid/static/js/table.js
@@ -45,7 +45,24 @@ $(document).ready(function () {
             { data: "cases", className: "text-right" },
             { data: "new-cases", className: "text-right" }
         ],
-    });
-   
+    });   
+
+    $("#nursingHome").DataTable({
+        "searching": false,
+        "paging": false,
+        "info": false,
+        "filter":   false,
+        "order": [[ 0, "asc" ]],
+        columnDefs: [
+            { orderable: true, className: 'reorder', targets: 0 },
+            { orderable: false, targets: '_all' }
+        ],
+        columns: [
+            { data: "week-end", type: "date" },    
+            { data: "all-deaths", className: "text-right" },
+            { data: "covid-deaths", className: "text-right" },
+            { data: "pct-covid", className: "text-right" }
+        ]
+    });   
 });
 
